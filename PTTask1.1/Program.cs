1cec09427374c41772b6da4c301d866a08527b60
xaliqovcelal@gmail.com <xaliqovcelal@gmail.com>
2022-02-11 12:15:31 +0400

    Add project files.

---
+++ PTTask1.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTTask1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        HomePage:
+            Console.WriteLine("Please select a currency type: \n1-USD\n2-EUR\n3-TL");
+            int selected_currency = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the amount");
+            double amount_entered = double.Parse(Console.ReadLine());
+            switch (selected_currency)
+            {
+                case 1:
+                    ValutaExchanger.Exchange(CurrencyType.USD, amount_entered);
+                    break;
+                case 2:
+                    ValutaExchanger.Exchange(CurrencyType.EUR, amount_entered);
+                    break;
+                case 3:
+                    ValutaExchanger.Exchange(CurrencyType.TL, amount_entered);
+                    break;
+            }
+            Console.WriteLine("------------------------------------------------");
+            goto HomePage;
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PTTask1.1/ValutaExchanger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PTTask1._1
+{
+    public static class ValutaExchanger
+    {
+        public static void Exchange(CurrencyType currency, double sum)
+        {
+            double result1;
+            double result2;
+            Dictionary<CurrencyType, double> Currencies = new Dictionary<CurrencyType, double>();
+            if (currency == CurrencyType.EUR)
+            {
+                Currencies.Add(CurrencyType.USD, result1 = sum * 1.1458);
+                Currencies.Add(CurrencyType.TL, result2 = sum * 15.4838);
+                Console.WriteLine($"USD equivalent {result1}\nTL equivalent {result2}");
+            }
+            if (currency == CurrencyType.USD)
+            {
+                Currencies.Add(CurrencyType.EUR, result1 = sum * 0.8727);
+                Currencies.Add(CurrencyType.TL, result2 = sum * 13.4642);
+                Console.WriteLine($"EUR equivalent {result1}\nTL equivalent {result2}");
+            }
+            if (currency == CurrencyType.TL)
+            {
+                Currencies.Add(CurrencyType.EUR, result1 = sum * 0.0646);
+                Currencies.Add(CurrencyType.USD, result2 = sum * 0.0743);
+                Console.WriteLine($"EUR equivalent {result1}\nUSD equivalent {result2}");
+            }
+        }
+    }
+    public enum CurrencyType
+    {
+        USD,
+        EUR,
+        TL
+    }
+}
